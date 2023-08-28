6bf2a45a6718480633cf263f300d05af8f513606
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2023-08-28 12:31:12 +0200

    chore!: update to typescript-eslint v6
    BREAKING CHANGE: users need to remove typescript-eslint v5

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -65,9 +65,8 @@ module.exports = {
             files        : [ "*.ts" ],
             parser       : "@typescript-eslint/parser",
             extends: [
-                "plugin:@typescript-eslint/recommended",
-                "plugin:@typescript-eslint/recommended-requiring-type-checking",
-                //"plugin:@typescript-eslint/strict"
+                "plugin:@typescript-eslint/strict-type-checked",
+                "plugin:@typescript-eslint/stylistic-type-checked",
             ],
             rules: {
                 "indent"                                 : [ "off" ],
